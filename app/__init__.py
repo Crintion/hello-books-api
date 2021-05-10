8f1a2478504bfe64230945bf70f002e32801c695
Christian <Codedreamer20@gmail.com>
2021-05-10 09:08:38 -0700

    Add Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenf import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -9,18 +11,23 @@ migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
-
     # DB Config
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    if not test_config:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+        
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+    
     
     db.init_app(app)
     migrate.init_app(app, db)
+
     from app.models.book import Book
 
-    from .routes import hello_world_bp, books_bp
-    #app.register_blueprint(hello_world_bp)
+    from .routes import books_bp
     app.register_blueprint(books_bp)
 
 
--- app/routes.py
+++ app/routes.py
@@ -3,49 +3,66 @@ from app import db
 from app.models.book import Book
 from flask import request, Blueprint, make_response, jsonify
 
-hello_world_bp = Blueprint("hello_world", __name__)
-
-# @hello_world_bp.route('/hello-world', methods=["GET"])
-# def get_hello_world():
-#     my_response = "Hello, World!"
-#     return my_response
-
-# @hello_world_bp.route('/hello-world/JSON', methods=["GET"])
-# def hello_world_json():
-#     return {
-#         "name": "Crintion!",
-#         "message": "Heya!",
-#         "hobbies": ["Coding, loving Fissick"]
-#     }, 201
-
-# @hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-# def broken_endpoint():
-#     response_body = {
-#         "name": "Ada Lovelace",
-#         "message": "Hello!",
-#         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-#     }
-#     new_hobby = "Surfing"
-#     response_body["hobbies"].append(new_hobby)
-#     return response_body
-
 books_bp = Blueprint('books', __name__, url_prefix='/books')
-@books_bp.route('', methods=['GET', 'POST'])
+
+def is_int(value):
+    try:
+        return int(value)
+    except ValueError:
+        return False
+
+@books_bp.route("/<book_id>", methods=["GET"], strict_slashes=False)
+def get_single_book(book_id):
+    book = Book.query.get(book_id)
+    #Try to find the book with the given id
+    if not is_int(book_id):
+        return {
+            "message": f"ID {book_id} must be an integer",
+            "success": False
+        }, 400
+    
+    elif book:
+        return book.to_json(), 200
+
+    return {
+        'message': f'Book with id {book_id} was not found',
+        'success': False
+    }, 404
+
+@books_bp.route('', methods=['GET'], strict_slashes=False)
+def books_index():
+    books = Book.query.all()
+    books_response = []
+    for book in books:
+        books_response.append(book.to_json() )
+
+    return jsonify(books_response), 200
+
+@books_bp.route('', methods=['POST'], strict_slashes=False)
 def handle_books():
-    if request.method == 'GET':
-        books = Book.query.all()
-        books_response = []
-        for book in books:
-            books_response.append({
-                'id': book.id,
-                'title': book.title,
-                'description': book.description
-            })
-        return jsonify(books_response)
-    elif request.method == 'POST':
-        request_body = request.get_json()
-        new_book = Book(title=request_body['title'],
+    request_body = request.get_json()
+    new_book = Book(title=request_body['title'],
                         description=request_body['description'])
-        db.session.add(new_book)
+    db.session.add(new_book)
+    db.session.commit()
+    return {
+        "success": True,
+        "message": f'Book {new_book.title} has been created!!'
+    }, 201
+
+@books_bp.route("/<book_id>", methods=["GET", "PUT"])
+def handle_book(book_id):
+    book = Book.query.get(book_id)
+
+    if request.method == "GET":
+        # ... existing code that returned a dictionary
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        book.title = form_data["title"]
+        book.description = form_data["description"]
+
         db.session.commit()
-        return make_response(f'Book {new_book.title} successfully created', 201)
+
+        return make_response(f"Book #{book.id} successfully updated")
+         
